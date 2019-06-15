5bf0579fb2db5a527c58021c7856fc5f5b5542ad
Huy Pham <phamhm@gmail.com>
2019-06-15 11:00:11 -0700

    remove checking for iterable abc.

--- algorithms/arrays/flatten.py
+++ algorithms/arrays/flatten.py
@@ -3,18 +3,22 @@ Implement Flatten Arrays.
 Given an array that may contain nested arrays,
 produce a single resultant array.
 """
-from collections import Iterable
-
 
 # return list
 def flatten(input_arr, output_arr=None):
+    if not input_arr:
+        return output_arr
+
     if output_arr is None:
         output_arr = []
     for ele in input_arr:
-        if isinstance(ele, Iterable):
-            flatten(ele, output_arr)    #tail-recursion
-        else:
-            output_arr.append(ele)      #produce the result
+        try:
+            iter(ele)
+            if ele:
+                output_arr.append(ele[0])
+                flatten(ele[1:], output_arr)
+        except TypeError:
+            output_arr.append(ele)
     return output_arr
 
 
@@ -25,7 +29,8 @@ def flatten_iter(iterable):
     returns generator which produces one dimensional output.
     """
     for element in iterable:
-        if isinstance(element, Iterable):
-            yield from flatten_iter(element)    
-        else:
+        try:
+            iter(element)
+            yield from flatten_iter(element)
+        except TypeError:
             yield element
